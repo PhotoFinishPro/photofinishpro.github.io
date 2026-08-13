3ae84e7d936129ced223456c83eb38525b279c0f
PhotoFinishPro <photofinishprotiming@gmail.com>
2026-08-13 16:55:35 -0700

    Update pricing.js

--- pricing.js
+++ pricing.js
@@ -19,7 +19,7 @@ async function loadJsonFile() {
             console.log(sportsList);
             var innerHTMLForSelect = "<option> -- Please Select a Sport -- </option>";
             for (var i = 0; i < sportsList.length; i++){
-                  innerHTMLForSelect += "<option value='" + sportsList[i] + "'>" + sportsList[i] +" (" + sportsAltList[i]  ")</option>";
+                  innerHTMLForSelect += "<option value='" + sportsList[i] + "'>" + sportsList[i] +" (" + sportsAltList[i] + ")</option>";
             }
             document.getElementById("select").innerHTML = innerHTMLForSelect;
             var test = jsonData.TF[0].Packages[0].name;
