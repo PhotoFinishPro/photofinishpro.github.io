62136abd13e6d407c34464db6394588fc20cfabb
PhotoFinishPro <photofinishprotiming@gmail.com>
2026-08-21 15:15:51 -0700

    Update pricing.js

--- pricing.js
+++ pricing.js
@@ -42,7 +42,10 @@ async function loadJsonFile() {
                         if(jsonData.TF[0].Definitions[0].disclaimers != null){
                               currentTableHTML += "<tr><td colspan ='4' style='color:red'>" + jsonData.TF[0].Definitions[0].disclaimers +"</td></tr>"
                         }
-                        currentTableHTML += "<tr><td colspan='4'><em>Packages:</em></td></tr>";
+                        currentTableHTML += "<tr><td colspan='4'><em>Packages:</em></td></tr><tr><td>Name:</td><td>Price:</td><td>Features:</td><td>Sample:</td></tr>";
+                        for(var i = 0; i < jsonData.TF[0].Packages.length; i++){
+                              currentTableHTML += "<tr><td>" + jsonData.TF[0].Packages[i].name + "</td><td>$" + jsonData.TF[0].Packages[i].price + "</td><td>" + jsonData.TF[0].Packages[i].description + "</td><td>" + jsonData.TF[0].Packages[i].sample + "</td></tr>
+                        }
                         console.log("TF PRICING");
                         
                   } else if (selectedValue == "Cross Country Races (XC)"){
