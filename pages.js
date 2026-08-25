8ccb3ee991d2d854dc2bf0d5979ef09cf3c93702
PhotoFinishPro <photofinishprotiming@gmail.com>
2026-08-25 16:17:45 -0700

    Create pages.js

---
+++ pages.js
@@ -0,0 +1,19 @@
+var menu = document.getElementById("menu");
+var currentMenuHtml = "";
+async function loadJsonFile() {
+  try {
+    // Fetch the JSON file (relative path)
+    const response = await fetch('pages.json');
+
+    // Check if the request was successful
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Parse the JSON response into a JavaScript object
+    const jsonData = await response.json();
+    for(var i = 0; i < jsonData.pages.length; i++){
+      currentMenuHtml += "<li><a href='" + jsonData.pages[i].link + "'>" + jsonData.pages[i].title + "</a></li>";
+    }
+    menu.innerHTML = currentMenuHtml;
+  }
