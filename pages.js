92a1eb3b4cdab684cb7da66d14f2a99d122f77fe
PhotoFinishPro <photofinishprotiming@gmail.com>
2026-08-25 16:58:11 -0700

    Update pages.js

--- pages.js
+++ pages.js
@@ -9,11 +9,17 @@ async function loadJsonFile() {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-
     // Parse the JSON response into a JavaScript object
     const jsonData = await response.json();
+    const path = window.location.pathname;
+    var pathList = path.split("/");
+    console.log(pathList);
     for(var i = 0; i < jsonData.pages.length; i++){
-      currentMenuHtml += "<li><a href='" + jsonData.pages[i].link + "' target='" + jsonData.pages[i].target + "'>" + jsonData.pages[i].title + "</a></li>";
+      if(pathList[1] == jsonData.pages[i].link){
+        currentMenuHtml += "<li style='color:lightgrey'><a href='" + jsonData.pages[i].link + "' target='" + jsonData.pages[i].target + "'>" + jsonData.pages[i].title + "</a></li>";
+      } else {
+        currentMenuHtml += "<li><a href='" + jsonData.pages[i].link + "' target='" + jsonData.pages[i].target + "'>" + jsonData.pages[i].title + "</a></li>";
+      }
     }
     menu.innerHTML = currentMenuHtml;
   console.log('Loaded JSON data:', jsonData);
